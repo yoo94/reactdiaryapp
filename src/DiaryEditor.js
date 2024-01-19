c0a3aca60157d3c6e1c1d57a5b8c8b38da2e453a
yoo94 <jaeseok9405@gmail.com>
2024-01-19 13:28:18 +0900

    일기 기본 폼 및 스테이트

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -5,6 +5,7 @@ const DiaryEditor = () => {
   const [state,setstate] = useState({
       author: "",
       content:"",
+      emotion: 1,
   });
 
   const handleChangeEvent = (e)=>{
@@ -16,6 +17,9 @@ const DiaryEditor = () => {
     })
 
   }
+  const handlesubmmit = ()=>{
+    console.log(state)
+  }
 
   return (
     <div className="DiaryEditor">
@@ -25,6 +29,19 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea name="content" value={state.content} onChange={handleChangeEvent}/>
+      </div>
+      <div>
+        <div>오늘의 감정점수  </div>
+        <select name="emotion" value={state.emotion} onChange={handleChangeEvent}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button name="save" onClick={handlesubmmit}>일기 저장</button>
       </div>
     </div>
   );
